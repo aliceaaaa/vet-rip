33804676f90f964ff9e8baa13ce6e83aa01b009c
alice <aandrianova@c-cars.tech>
2021-05-31 11:15:38 +0300

    Form prevent default

--- src/UI/Form/Form.jsx
+++ src/UI/Form/Form.jsx
@@ -6,10 +6,10 @@ const Form = ({ lang }) => {
   const formTexts = lang ? ['Ваше имя', 'Ваш телефон'] : ['Your name', 'Your phone number'];
 
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={(e) => e.preventDefault()}>
       <input name="name" placeholder={formTexts[0]} type="text" />
       <input type="tel" name="phone" placeholder={formTexts[1]} />
-      <Button lang={lang} />
+      <Button lang={lang} onClick={() => {}} />
     </form>
   );
 };
